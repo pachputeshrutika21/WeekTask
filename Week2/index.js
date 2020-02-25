824f115640d72046fa187c677df3b52f5ea65a1c
pachputeshrutika21 <52059643+pachputeshrutika21@users.noreply.github.com>
2020-02-25 10:01:45 +0530

    commit

--- Week2/index.js
+++ Week2/index.js
@@ -1,7 +1,20 @@
-
-let arr=[];
-for(let i=0;i<=100;i++)
+const arr=[];
+function isInsertNumber() 
 {
-    arr.push(i);
-}
+    for(let i=0;i<=100;i++)
+    {
+        arr.push(i);
+    }
 console.log(arr);
+}
+function isAccept()
+{
+    for(i=0;i<=100;i++)
+    {
+       arr.push(i);
+    }
+    for(i=0;i<arr.length;i++)
+    {
+        console.log(arr[i]);
+    }
+}
